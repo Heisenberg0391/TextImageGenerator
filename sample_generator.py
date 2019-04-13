94e5dbe882708341574281edac284bc3136763d9
Heisenberg0391 <dumdumslug@hotmail.com>
2019-04-13 15:21:31 +0800

    new synthetic engine

--- sample_generator.py
+++ sample_generator.py
@@ -1,219 +1,263 @@
 # -*- coding:utf8 -*-
-# 生成合成文本图像用于训练
+"""
+功能：将语料中的每行文本绘制成图像
+输入：语料文件
+输出：文本图像
+参数：在config.cfg.py中: config_path, corpus, dict, FONT_PATH
+"""
 import codecs
 import numpy as np
-import config.cfg as cfg
+import mycrnn_pc.config.cfg as cfg
 import os
 from PIL import Image, ImageDraw, ImageFont
-from scipy import ndimage
 import progressbar
-import tkinter as tk
-from tkinter import filedialog
+import glob
 import cv2
+from colormath.color_objects import CMYKColor,sRGBColor, LabColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
 
 
-def speckle(img):
-    severity = np.random.uniform(0, 0.6*255)
-    blur = ndimage.gaussian_filter(np.random.randn(*img.shape) * severity, 1)
-    img_speck = (img + blur)
-    img_speck[img_speck > 255] = 255
-    img_speck[img_speck <= 0] = 0
-    return img_speck
+def rotate_bound(image, angle, bg_color):
+    # grab the dimensions of the image and then determine the
+    # center
+    (h, w) = image.shape[:2]
+    (cX, cY) = (w // 2, h // 2)
 
+    # grab the rotation matrix (applying the negative of the
+    # angle to rotate clockwise), then grab the sine and cosine
+    # (i.e., the rotation components of the matrix)
+    M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+    cos = np.abs(M[0, 0])
+    sin = np.abs(M[0, 1])
 
-def augmentation(img, ):
-    # 不能直接在原始image上改动
-    image = img.copy()
-    img_h, img_w = img.shape
-    mode = np.random.randint(0, 9)
-    '''添加随机模糊和噪声'''
-    # 高斯模糊
-    if mode == 0:
-        image = cv2.GaussianBlur(image,(5, 5), np.random.randint(1, 10))
+    # compute the new bounding dimensions of the image
+    nW = int((h * sin) + (w * cos))
+    nH = int((h * cos) + (w * sin))
 
-    # 模糊后二值化，虚化边缘
-    if mode == 1:
-        image = cv2.GaussianBlur(image, (3, 3), np.random.randint(1, 9))
-        ret, th = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        thresh = image.copy()
-        thresh[thresh >= th] = 0
-        thresh[thresh < th] = 255
-        image = thresh
+    # adjust the rotation matrix to take into account translation
+    M[0, 2] += (nW / 2) - cX
+    M[1, 2] += (nH / 2) - cY
 
-    # 横线干扰
-    if mode == 2:
-        for i in range(0, img_w, 2):
-            cv2.line(image, (0, i), (img_w, i), 0, 1)
+    # perform the actual rotation and return the image
+    return cv2.warpAffine(image, M, (nW, nH),
+                          borderMode=cv2.BORDER_CONSTANT,
+                          borderValue=bg_color)
 
-    # 竖线
-    if mode == 3:
-        for i in range(0, img_w, 2):
-            cv2.line(image, (i, 0), (i, img_h), 0, 1)
-
-    # 十字线
-    if mode == 4:
-        for i in range(0, img_h, 2):
-            cv2.line(image, (0, i), (img_w, i), 0, 1)
-        for i in range(0, img_w, 2):
-            cv2.line(image, (i, 0), (i, img_h), 0, 1)
-
-    # 左右运动模糊
-    if mode == 5:
-        kernel_size = 5
-        kernel_motion_blur = np.zeros((kernel_size, kernel_size))
-        kernel_motion_blur[int((kernel_size - 1) / 2), :] = np.ones(kernel_size)
-        kernel_motion_blur = kernel_motion_blur / kernel_size
-        image = cv2.filter2D(image, -1, kernel_motion_blur)
-
-    # 上下运动模糊
-    if mode == 6:
-        kernel_size = 9
-        kernel_motion_blur = np.zeros((kernel_size, kernel_size))
-        kernel_motion_blur[:, int((kernel_size - 1) / 2)] = np.ones(kernel_size)
-        kernel_motion_blur = kernel_motion_blur / kernel_size
-        image = cv2.filter2D(image, -1, kernel_motion_blur)
-
-    # 高斯噪声
-    if mode == 7:
-        row, col = [img_h, img_w]
-        mean = 0
-        sigma = 1
-        gauss = np.random.normal(mean, sigma, (row, col))
-        gauss = gauss.reshape(row, col)
-        noisy = image + gauss
-        image = noisy.astype(np.uint8)
-
-    # 污迹
-    if mode == 8:
-        image = speckle(image)
-    return image
+def distort(input_img):
+    h, w, d = input_img.shape
+    rotated = cv2.getRotationMatrix2D((w / 2, h / 2), 15, 1)
+    return rotated
 
 
 class TextGenerator():
-    def __init__(self, img_w, img_h, aug):
+    def __init__(self, save_path):
         """初始化参数来自config文件
         """
-        self.aug = aug
-        self.img_w = img_w
-        self.img_h = img_h
-        self.img_lim = cfg.canvas_lim  # 图片最大宽度
         # 语料参数
+        self.max_row_len = cfg.max_row_len
+        self.max_label_len = cfg.max_label_len  # CTC最大输入长度
+        self.n_samples = cfg.n_samples
         self.dictfile = cfg.dict  # 字典
         self.dict = []
         self.corpus_file = cfg.corpus  # 语料集
+        self.save_path = save_path
         # 加载字体文件
         self.font_factor = 1  # 控制字体大小
-        # 选择字体
-        root = tk.Tk()
-        root.withdraw()
-        self.font_path = filedialog.askopenfilename()
+        # 加载字体文件
+        self.load_fonts()
         # 加载语料集
         self.build_dict()
-        self.build_train_list(cfg.n_samples, cfg.sentence_lim)
+        self.build_train_list(self.n_samples, self.max_row_len)
+
+
+    def load_fonts(self):
+        """ 加载字体文件并设定字体大小
+            TODO： 无需设定字体大小，交给pillow
+        :return: self.fonts
+        """
+        self.fonts = {}  # 所有字体
+        self.font_name = []  # 字体名，用于索引self.fonts
+        # 字体完整路径
+        font_path = os.path.join(cfg.FONT_PATH, "*.*")
+        # 获取全部字体路径，存成list
+        fonts = list(glob.glob(font_path))
+        # 遍历字体文件
+        for each in fonts:
+            # 字体大小
+            fonts_list = {}  # 每一种字体的不同大小
+            font_name = each.split('\\')[-1].split('.')[0]  # 字体名
+            self.font_name.append(font_name)
+            font_size = 60
+            for j in range(0, 10):  # 当前字体的不同大小
+                # 调整字体大小
+                cur_font = ImageFont.truetype(each, font_size, 0)
+                fonts_list[str(j)] = cur_font
+                font_size -= 2
+            self.fonts[font_name] = fonts_list
 
     def build_dict(self):
         """ 打开字典，加载全部字符到list
             每行是一个字
+        :return: self.dict
         """
         with codecs.open(self.dictfile, mode='r', encoding='utf-8') as f:
             # 按行读取语料
             for line in f:
-                # 当前行单词去除结尾，为了正常读取空格，第一行两个空格
+                # 当前行单词去除结尾
                 word = line.strip('\r\n')
                 # 只要没超出上限就继续添加单词
                 self.dict.append(word)
-        # 最后一位作为空白符
+        # 最后一类作为空白占位符
         self.blank_label = len(self.dict)
 
-    def mapping_list(self):
+    def mapping_list(self, DATASET_DIR):
         # 写图像文件名和类别序列的对照表
-        file_path = os.path.join(cfg.OUTPUT_DIR, 'map_list.txt')
+        file_path = os.path.join(DATASET_DIR, 'map_list.txt')
         with codecs.open(file_path, mode='w', encoding='utf-8') as f:
             for i in range(len(self.train_list)):
-                f.write("{}.png {} \n".format(i, self.train_list[i]))
+                f.write("{}.jpeg {} \n".format(i, self.train_list[i]))
 
-    def build_train_list(self, num_rows, max_row_len=None):
+    def build_train_list(self, n_samples, max_row_len=None):
         # 过滤语料，留下适合的内容组成训练list
-        assert max_row_len <= self.img_lim
-        self.num_rows = num_rows
-        self.max_row_len = max_row_len
-        sentence_list = []
+        print('正在加载语料...')
+        assert max_row_len <= self.max_label_len  # 最大类别序列长度
+        self.n_samples = n_samples  # 语料总行数
+        sentence_list = []  # 存放每行句子
         self.train_list = []
+        self.label_len = [0] * self.n_samples  # 类别序列长度
+        self.label_sequence = np.ones([self.n_samples, self.max_label_len]) * -1  # 类别ID序列
 
         with codecs.open(self.corpus_file, mode='r', encoding='utf-8') as f:
             # 按行读取语料
-            for line in f:
-                sentence = line.rstrip().replace(' ', '')  # 当前行单词
-                if len(sentence) <= max_row_len and len(sentence_list) < num_rows:
-                    # 只要句子长度不超过画布上限且句子数量没超出上限就继续添加
+            for sentence in f:
+                sentence = sentence.strip()  # 去除行末回车
+                if len(sentence_list) < n_samples:
+                    # 只要长度和数量没超出上限就继续添加单词
                     sentence_list.append(sentence)
-                elif len(sentence) > max_row_len and len(sentence_list) < num_rows:
-                    # 截断句子
-                    sentence_list.append(sentence[0:max_row_len])
 
-        if len(sentence_list) < self.num_rows:
-            raise IOError('语料不够')
+        np.random.shuffle(sentence_list)  # 打乱语料
+        if len(sentence_list) < self.n_samples:
+            raise IOError('语料不足')
 
+        # 遍历语料中的每一句(行)
         for i, sentence in enumerate(sentence_list):
-            # 遍历语料中的每一句(行)
+            # 每个句子的长度
+            label_len = len(sentence)
+            filted_sentence = ''
             # 将单词分成字符，然后找到每个字符对应的整数ID list
+            # n_samples个样本每个一行max_row_len元向量(单词最大长度)，每一元为该字符的整数ID
             label_sequence = []
-            for j, word in enumerate(sentence):  # 检查句子中是否包含生僻字
-                try:
-                    index = self.dict.index(word)
-                    label_sequence.append(index)
-                except ValueError:
-                    print("字典不包含：{}，已忽略".format(word))
-                    sentence_list[i] = sentence_list[i][0:j] + sentence_list[i][j+1:]  # 从该句中删除生僻字
+            for j, word in enumerate(sentence):
+                index = self.dict.index(word)
+                label_sequence.append(index)
+                filted_sentence += word
 
+            if filted_sentence is not '':
+                # 当前样本的类别序列及其长度
+                self.label_len[i] = label_len
+                self.label_sequence[i, 0:self.label_len[i]] = label_sequence
+            else:  # 单独处理空白样本
+                self.label_len[i] = 1
+                self.label_sequence[i, 0:self.label_len[i]] = self.blank_label  # 空白符
+
+        self.label_sequence = self.label_sequence.astype('int')
         self.train_list = sentence_list  # 过滤后的训练集
-        np.random.shuffle(self.train_list)  # 打乱顺序
-        self.mapping_list()  # 保存图片名和类别序列的 map list
+        self.mapping_list(self.save_path)  # 保存图片名和类别序列的 map list
 
     def paint_text(self, text, i):
         """ 使用PIL绘制文本图像，传入画布尺寸，返回文本图像
-        :param: text 当前行文本
-        :param: i 当前文本在训练集中的序号
+        :param h: 画布高度
+        :param w: 画布宽度
+        :return: img
         """
-        factor = 1  # 初始字体大小
-        # 创建画布
-        canvas = np.zeros(shape=(self.img_h, self.img_w), dtype=np.uint8)
-        canvas[0:] = 255
-        # 转换图像模式，保证合成的两张图尺寸模式一致
-        ndimg = Image.fromarray(canvas).convert('RGBA')
-        draw = ImageDraw.Draw(ndimg)
+        # 创建画布背景
+        bg_b = np.random.randint(0, 255)  # 背景色
+        bg_g = np.random.randint(0, 255)
+        bg_r = np.random.randint(0, 255)
+        # 前景色
+        fg_b = np.random.randint(0, 255)  # 背景色
+        fg_g = np.random.randint(0, 255)
+        fg_r = np.random.randint(0, 255)
+        # 计算前景和背景的彩色相似度
+        bg_color = sRGBColor(bg_b, bg_g, bg_r)
+        bg_color = convert_color(bg_color, CMYKColor)  # 转cmyk
+        bg_color = convert_color(bg_color, LabColor)
+        fg_color = sRGBColor(fg_b, fg_g, fg_r)
+        fg_color = convert_color(fg_color, CMYKColor)  # 转cmyk
+        fg_color = convert_color(fg_color, LabColor)
+        delta_e = delta_e_cie2000(bg_color, fg_color)
+        while delta_e < 150 and delta_e > 250:  # 150-250
+            # 创建画布背景色
+            bg_b = np.random.randint(0, 255)
+            bg_g = np.random.randint(0, 255)
+            bg_r = np.random.randint(0, 255)
+            # 文字前景色
+            fg_b = np.random.randint(0, 255)
+            fg_g = np.random.randint(0, 255)
+            fg_r = np.random.randint(0, 255)
+            # 计算前景和背景的彩色相似度
+            bg_color = sRGBColor(bg_b, bg_g, bg_r)
+            bg_color = convert_color(bg_color, LabColor)
+            fg_color = sRGBColor(fg_b, fg_g, fg_r)
+            fg_color = convert_color(fg_color, LabColor)
+            delta_e = delta_e_cie2000(bg_color, fg_color)
 
-        font = ImageFont.truetype(self.font_path, int(img_h * factor), 0)  # 加载字体
-        text_size = font.getsize(text)  # 获取当前字体下的文本区域大小
+        # 随机选择字体
+        np.random.shuffle(self.font_name)
+        cur_fonts = self.fonts.get(self.font_name[0])
+        keys = list(cur_fonts.keys())
+        np.random.shuffle(keys)
+        font = cur_fonts.get(keys[0])
+        text_size = font.getsize(text)
 
-        # 自动调整字体大小避免超出边界, 至少留白水平20%
-        margin = [self.img_w - int(0.2 * self.img_w), self.img_h - int(0.2 * self.img_h)]
-        while (text_size[0] > margin[0]) or (text_size[1] > margin[1]):
-            factor -= 0.01  # 控制字体大小
-            font = ImageFont.truetype(self.font_path, int(img_h * factor), 0)  # 加载字体
-            text_size = font.getsize(text)
+        # 根据字体大小创建画布
+        img_w = text_size[0]
+        img_h = text_size[1]
+
+        # 文本区域上限
+        h_space = np.random.randint(6, 20)
+        w_space = 6
+        h = img_h + h_space
+        w = img_w + w_space
+        canvas = Image.new('RGB', (w, h), (bg_b, bg_g, bg_r))
+        draw = ImageDraw.Draw(canvas)
 
         # 随机平移
-        horizontal_space = self.img_w - text_size[0]
-        vertical_space = self.img_h - text_size[1]
-        start_x = np.random.randint(2, horizontal_space-2)
-        start_y = np.random.randint(2, vertical_space-2)
+        start_x = np.random.randint(2, w_space-2)
+        start_y = np.random.randint(2, h_space-2)
 
         # 绘制当前文本行
-        draw.text((start_x, start_y), text, font=font, fill=(0, 0, 0, 255))
-        img_array = np.array(ndimg)
+        draw.text((start_x, start_y), text, font=font, fill=(fg_b, fg_g, fg_r))
+        img_array = np.array(canvas)
+        # 透视失真
+        src = np.float32([[start_x, start_y],
+                          [start_x + w, start_y],
+                          [start_x + w, start_y + h],
+                          [start_x, start_y + h]])
 
-        # 转灰度图
-        grey_img = img_array[:, :, 0]  # [32, 256, 4]
-        if self.aug == True:
-            auged = augmentation(grey_img)
-            ndimg = Image.fromarray(auged).convert('RGBA')
-        # 画图看一下
-        # ndimg.show()
-        save_path = os.path.join(cfg.OUTPUT_DIR, '{}.png'.format(i))  # 类别序列即文件名
+        dst = np.float32([[start_x + np.random.randint(0, 10), start_y + np.random.randint(0, 5)],
+                          [start_x + w - np.random.randint(0, 10), start_y + np.random.randint(0, 5)],
+                          [start_x + w - np.random.randint(0, 10), start_y + h - np.random.randint(0, 5)],
+                          [start_x + np.random.randint(0, 10), start_y + h - np.random.randint(0, 5)]])
+        M = cv2.getPerspectiveTransform(src, dst)
+        img_array = cv2.warpPerspective(img_array.copy(), M, (w, h),
+                                  borderMode=cv2.BORDER_CONSTANT,
+                                  borderValue=(bg_b, bg_g, bg_r))
+        # Image.fromarray(img_array).show()
+        # 随机旋转
+        angle = np.random.randint(-8, 8)
+        rotated = rotate_bound(img_array, angle=angle, bg_color=(bg_b, bg_g, bg_r))
+        canvas = Image.fromarray(rotated)
+        img_array = np.array(canvas.convert('CMYK'))[:,:,0:3]  # rgb to cmyk
+        img_array = cv2.resize(img_array.copy(), (128, 32), interpolation=cv2.INTER_CUBIC)  # resize
+
+        ndimg = Image.fromarray(img_array).convert('CMYK')
+        # 保存
+        save_path = os.path.join(self.save_path, '{}.jpeg'.format(i))  # 类别序列即文件名
         ndimg.save(save_path)
 
-    def text_generator(self):
+    def generator(self):
         n_samples = len(self.train_list)
         # 进度条
         widgets = ["数据集创建中: ", progressbar.Percentage(), " ", progressbar.Bar(), " ", progressbar.ETA()]
@@ -229,12 +273,14 @@ class TextGenerator():
 
 if __name__ == '__main__':
     np.random.seed(0)  # 决定训练集的打乱情况
+    DATASET_DIR = 'imageset1'
     # 输出路径
-    if not os.path.exists(cfg.OUTPUT_DIR):
-        os.makedirs(cfg.OUTPUT_DIR)
-    # 参数
-    img_w = 300
-    img_h = 40
+    if not os.path.exists(DATASET_DIR):
+        os.makedirs(DATASET_DIR)
+    psnr = []
+    ssim = []
     # 实例化图像生成器
-    img_gen = TextGenerator(img_w=img_w, img_h=img_h, aug=True)  # aug参数控制是否使用数据增强
-    img_gen.text_generator()
+    if not os.path.exists(DATASET_DIR):
+        os.makedirs(DATASET_DIR)
+    img_gen = TextGenerator(save_path=DATASET_DIR)
+    img_gen.generator()
