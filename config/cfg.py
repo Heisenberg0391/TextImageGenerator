1d481340887b84e66fbc80807f87f6d23ed1d6a6
heisenberg <dumdumslug@hotmail.com>
2021-06-27 16:52:22 +0800

    update cfg

--- config/cfg.py
+++ config/cfg.py
@@ -7,7 +7,7 @@ OUTPUT_DIR = 'outputs'
 import os
 # 路径参数
 config_path = 'config'
-corpus = os.path.join(config_path, 'corpus/sentences.txt')  # 语料集
+corpus = os.path.join(config_path, 'corpus/corpus.txt')  # 语料集
 dict = os.path.join(config_path, 'corpus/dict.txt')  # 字典文件
 FONT_PATH = 'fonts'  # 字体文件路径
 n_samples = 16000
